b2a9b77ea14a91af8632add74c9bcc2aa13b9b1d
a-bombarda <andrea.bombarda@unibg.it>
2020-04-28 17:50:55 +0200

    Tests on Special Operations

--- gui/tests/test_changeValues.py
+++ gui/tests/test_changeValues.py
@@ -23,7 +23,7 @@ def test_changePSV_RR(qtbot):
     qtbot.addWidget(esp32)
 
     assert config is not None
-        
+
     print(esp32)
 
     window = MainWindow(config, esp32)
@@ -62,113 +62,58 @@ def test_changePSV_RR(qtbot):
         oldValue = i
         i = i - int(config['respiratory_rate']['step'])
         assert window.settings._all_spinboxes['respiratory_rate'].value() >= config['respiratory_rate']['min']
-#
-#
-# """
-# TS22
-# """
-# def test_changePSV_IE(qtbot):
-#     '''
-#     Test the change of the I:E
-#     '''
-#
-#     assert qt_api.QApplication.instance() is not None
-#
-#     esp32 = FakeESP32Serial(config)
-#     qtbot.addWidget(esp32)
-#
-#     assert config is not None
-#
-#     print(esp32)
-#
-#     window = MainWindow(config, esp32)
-#     qtbot.addWidget(window)
-#     window.show()
-#     qtbot.mouseClick(window.button_new_patient, QtCore.Qt.LeftButton)
-#     qtbot.mouseClick(window.button_start_vent, QtCore.Qt.LeftButton)
-#     assert window.bottombar.currentWidget() == window.toolbar
-#
-#     # Enter the menu and the PSV Settings tab
-#     qtbot.mouseClick(window.button_menu, QtCore.Qt.LeftButton)
-#     assert window.bottombar.currentWidget() == window.menu
-#     qtbot.mouseClick(window.button_settingsfork, QtCore.Qt.LeftButton)
-#     assert window.bottombar.currentWidget() == window.settingsfork
-#     qtbot.mouseClick(window.button_settings, QtCore.Qt.LeftButton)
-#     assert window.toppane.currentWidget() == window.settings
-#
-#     # Try to increase the value
-#     startingValue = window.settings._all_spinboxes['insp_expir_ratio'].value()
-#
-#     i = startingValue
-#     oldValue = 0
-#     while i <= int(config['insp_expir_ratio']['max'] + 1) or i == oldValue:
-#         window._start_stop_worker._settings.update_spinbox_value('insp_expir_ratio', i)
-#         oldValue = i
-#         i = i + int(config['insp_expir_ratio']['step'])
-#         assert window.settings._all_spinboxes['insp_expir_ratio'].value() <= config['insp_expir_ratio']['max']
-#
-#     # Try to decrease the value
-#     window._start_stop_worker._settings.update_spinbox_value('insp_expir_ratio', startingValue)
-#
-#     i = startingValue
-#     oldValue = 0
-#     while i >= int(config['insp_expir_ratio']['min'] - 1) or i == oldValue:
-#         window._start_stop_worker._settings.update_spinbox_value('insp_expir_ratio', i)
-#         oldValue = i
-#         i = i - int(config['insp_expir_ratio']['step'])
-#         assert window.settings._all_spinboxes['insp_expir_ratio'].value() >= config['insp_expir_ratio']['min']
-#
-#
-# """
-# TS23
-# """
-# def test_changePSV_PINSP(qtbot):
-#     '''
-#     Test the change of the Pinsp
-#     '''
-#
-#     assert qt_api.QApplication.instance() is not None
-#
-#     esp32 = FakeESP32Serial(config)
-#     qtbot.addWidget(esp32)
-#
-#     assert config is not None
-#
-#     print(esp32)
-#
-#     window = MainWindow(config, esp32)
-#     qtbot.addWidget(window)
-#     window.show()
-#     qtbot.mouseClick(window.button_new_patient, QtCore.Qt.LeftButton)
-#     qtbot.mouseClick(window.button_start_vent, QtCore.Qt.LeftButton)
-#     assert window.bottombar.currentWidget() == window.toolbar
-#
-#     # Enter the menu and the PSV Settings tab
-#     qtbot.mouseClick(window.button_menu, QtCore.Qt.LeftButton)
-#     assert window.bottombar.currentWidget() == window.menu
-#     qtbot.mouseClick(window.button_settingsfork, QtCore.Qt.LeftButton)
-#     assert window.bottombar.currentWidget() == window.settingsfork
-#     qtbot.mouseClick(window.button_settings, QtCore.Qt.LeftButton)
-#     assert window.toppane.currentWidget() == window.settings
-#
-#     # Try to increase the value
-#     startingValue = window.settings._all_spinboxes['insp_pressure'].value()
-#
-#     i = startingValue
-#     oldValue = 0
-#     while i <= int(config['insp_pressure']['max'] + 1) or i == oldValue:
-#         window._start_stop_worker._settings.update_spinbox_value('insp_pressure', i)
-#         oldValue = i
-#         i = i + int(config['insp_pressure']['step'])
-#         assert window.settings._all_spinboxes['insp_pressure'].value() <= config['insp_pressure']['max']
-#
-#     # Try to decrease the value
-#     window._start_stop_worker._settings.update_spinbox_value('insp_pressure', startingValue)
-#
-#     i = startingValue
-#     oldValue = 0
-#     while i >= int(config['insp_pressure']['min'] - 1) or i == oldValue:
-#         window._start_stop_worker._settings.update_spinbox_value('insp_pressure', i)
-#         oldValue = i
-#         i = i - int(config['insp_pressure']['step'])
-#         assert window.settings._all_spinboxes['insp_pressure'].value() >= config['insp_pressure']['min']
+
+
+"""
+TS22
+"""
+def test_changePSV_PINSP(qtbot):
+    '''
+    Test the change of the Pinsp
+    '''
+
+    assert qt_api.QApplication.instance() is not None
+
+    esp32 = FakeESP32Serial(config)
+    qtbot.addWidget(esp32)
+
+    assert config is not None
+
+    print(esp32)
+
+    window = MainWindow(config, esp32)
+    qtbot.addWidget(window)
+    window.show()
+    qtbot.mouseClick(window.button_new_patient, QtCore.Qt.LeftButton)
+    qtbot.mouseClick(window.button_start_vent, QtCore.Qt.LeftButton)
+    assert window.bottombar.currentWidget() == window.toolbar
+
+    # Enter the menu and the PSV Settings tab
+    qtbot.mouseClick(window.button_menu, QtCore.Qt.LeftButton)
+    assert window.bottombar.currentWidget() == window.menu
+    qtbot.mouseClick(window.button_settingsfork, QtCore.Qt.LeftButton)
+    assert window.bottombar.currentWidget() == window.settingsfork
+    qtbot.mouseClick(window.button_settings, QtCore.Qt.LeftButton)
+    assert window.toppane.currentWidget() == window.settings
+
+    # Try to increase the value
+    startingValue = window.settings._all_spinboxes['insp_pressure'].value()
+
+    i = startingValue
+    oldValue = 0
+    while i <= int(config['insp_pressure']['max'] + 1) or i == oldValue:
+        window._start_stop_worker._settings.update_spinbox_value('insp_pressure', i)
+        oldValue = i
+        i = i + int(config['insp_pressure']['step'])
+        assert window.settings._all_spinboxes['insp_pressure'].value() <= config['insp_pressure']['max']
+
+    # Try to decrease the value
+    window._start_stop_worker._settings.update_spinbox_value('insp_pressure', startingValue)
+
+    i = startingValue
+    oldValue = 0
+    while i >= int(config['insp_pressure']['min'] - 1) or i == oldValue:
+        window._start_stop_worker._settings.update_spinbox_value('insp_pressure', i)
+        oldValue = i
+        i = i - int(config['insp_pressure']['step'])
+        assert window.settings._all_spinboxes['insp_pressure'].value() >= config['insp_pressure']['min']
