aa0141bd47fe84c00251d8d205196d52d722fd51
a-bombarda <andrea.bombarda@unibg.it>
2020-04-29 11:02:42 +0200

    Test fixed

--- gui/tests/test_peep.py
+++ gui/tests/test_peep.py
@@ -3,31 +3,9 @@
 # from pytestqt import qt_compat
 from pytestqt.qt_compat import qt_api
 import pytest
-from communication.peep import peep
+from communication.peep import PEEP
 from mainwindow import MainWindow
 from communication.fake_esp32serial import FakeESP32Serial
-
-"""
-TH11
-"""
-def test_peepReset(qtbot):
-    '''
-    Test the reset of the Peep
-    '''
-
-    p = PEEP()
-
-    # Set a negative value for the t5 parameter, so the time t is obiously greater than t5
-    t5 = p.t5
-    p.t5 = -1
-    t0 = p.t0
-
-    # Force the flow
-    p.flow()
-
-    assert p.t0 != t0
-
-    p.t5 = t5
 
 
 """
