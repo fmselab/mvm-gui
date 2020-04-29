f729fdccb12c93f79762de45c588ff3d5b8b2fea
a-bombarda <andrea.bombarda@unibg.it>
2020-04-29 12:53:39 +0200

    Frozen plots test

--- gui/tests/test_basicMenu.py
+++ gui/tests/test_basicMenu.py
@@ -87,3 +87,53 @@ def test_start_operating_PSV(qtbot):
     assert window.button_autoassist.isEnabled() == False
 
     assert window._start_stop_worker.is_running()
+
+
+"""
+TS27
+"""
+def test_changeMode(qtbot):
+    assert qt_api.QApplication.instance() is not None
+
+    esp32 = FakeESP32Serial(config)
+    qtbot.addWidget(esp32)
+
+    assert config is not None
+
+    print(esp32)
+
+    window = MainWindow(config, esp32)
+    qtbot.addWidget(window)
+    window.show()
+    qtbot.mouseClick(window.button_new_patient, QtCore.Qt.LeftButton)
+    qtbot.mouseClick(window.button_start_vent, QtCore.Qt.LeftButton)
+    assert window.bottombar.currentWidget() == window.toolbar
+
+    assert "Stopped" in window._start_stop_worker._toolbar.label_status.text() and "PCV" in window._start_stop_worker._toolbar.label_status.text()
+
+    # Enter the menu and change the mode, without starting the ventilator
+    qtbot.mouseClick(window.button_menu, QtCore.Qt.LeftButton)
+    assert window.bottombar.currentWidget() == window.menu
+    qtbot.mouseClick(window.button_autoassist, QtCore.Qt.LeftButton)
+
+    assert "Stopped" in window._start_stop_worker._toolbar.label_status.text() and "PSV" in window._start_stop_worker._toolbar.label_status.text()
+
+
+"""
+TS28
+"""
+def test_wrongConfigurationFile(qtbot):
+    try:
+        assert qt_api.QApplication.instance() is not None
+
+        wrongConfigurationFile()
+
+        esp32 = FakeESP32Serial(config)
+        qtbot.addWidget(esp32)
+    except FileNotFoundError:
+        pass
+    except:
+        assert False
+
+
+
