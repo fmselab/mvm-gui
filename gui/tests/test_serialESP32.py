de104ccbe138ce88f6c24d569b93dc8bc50624f7
a-bombarda <andrea.bombarda@unibg.it>
2020-04-30 14:56:55 +0200

    New tests

---
+++ gui/tests/test_serialESP32.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+# from pytestqt import qt_compat
+from pytestqt.qt_compat import qt_api
+import pytest
+import time
+from .mvm_basics import *
+from communication.esp32serial import *
+import serial  # pySerial
+from PyQt5.QtCore import QCoreApplication
+
+"""
+TH24
+"""
+def test_newException(qtbot):
+    exception = ESP32Exception("Get", "1", "Prova")
+    assert exception.line == "1"
+
+
+"""
+TH25
+"""
+def test_newException(qtbot):
+    try:
+        seriale = ESP32Serial(config)
+        assert seriale.get_all_fields == config["get_all_fields"]
+    except serial.SerialException:
+        pass
+
+
