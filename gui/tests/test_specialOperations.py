73e7e0a3e65fccd2edb4c2018cb18e7976f386cd
a-bombarda <andrea.bombarda@unibg.it>
2020-04-30 16:09:04 +0200

    Lung Recruitment tests

--- gui/tests/test_specialOperations.py
+++ gui/tests/test_specialOperations.py
@@ -39,7 +39,6 @@ def test_expiratoryPause(qtbot):
     qtbot.mouseClick(window.specialbar.button_expause, QtCore.Qt.LeftButton)
     qtbot.waitUntil(lambda: not window.specialbar._timer['pause_exhale'].isActive(), timeout=10000)
 
-
 """
 TS25
 """
@@ -70,3 +69,35 @@ def test_inspiratoryPause(qtbot):
     qtbot.mouseClick(window.specialbar.button_inspause, QtCore.Qt.LeftButton)
     qtbot.waitUntil(lambda: not window.specialbar._timer['pause_inhale'].isActive(), timeout=10000)
 
+
+"""
+TS46
+"""
+def test_lungRecruitment(qtbot):
+    assert qt_api.QApplication.instance() is not None
+
+    esp32 = FakeESP32Serial(config)
+    qtbot.addWidget(esp32)
+
+    assert config is not None
+
+    print(esp32)
+
+    window = MainWindow(config, esp32)
+    qtbot.addWidget(window)
+    window.show()
+    qtbot.mouseClick(window.button_new_patient, QtCore.Qt.LeftButton)
+    qtbot.mouseClick(window.button_start_vent, QtCore.Qt.LeftButton)
+    assert window.bottombar.currentWidget() == window.toolbar
+
+    # Enter the menu and the Special Operations section
+    qtbot.mouseClick(window.button_menu, QtCore.Qt.LeftButton)
+    assert window.bottombar.currentWidget() == window.menu
+    qtbot.mouseClick(window.button_specialops, QtCore.Qt.LeftButton)
+    assert window.bottombar.currentWidget() == window.specialbar
+
+    # Click on the Country Specific Procedures button
+    qtbot.mouseClick(window.specialbar.button_lung_recruit, QtCore.Qt.LeftButton)
+    qtbot.mouseClick(window.specialbar._messagebar.button_confirm, QtCore.Qt.LeftButton)
+    qtbot.waitUntil(
+        lambda: "Lung Recruitment" in window.specialbar.button_lung_recruit.text(), timeout=3000)
