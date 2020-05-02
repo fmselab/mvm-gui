39c7872050766e92976ecd121c5c925275f5aaad
Marco Del Tutto <marco.deltutto@gmail.com>
2020-05-02 18:50:05 -0500

    Changing coefficients to be decreasing with x power

--- gui/communication/fake_esp32serial.py
+++ gui/communication/fake_esp32serial.py
@@ -425,7 +425,7 @@ class FakeESP32Serial(QtWidgets.QMainWindow):
                 Constructor
                 """
 
-                self._coefficients = [1, 1, 1, 1, 1]
+                self._coefficients = [10, 1e-1, 1e-2, 1e-3, 1e-4]
 
             def _flow(self, delta_p):
                 return self._coefficients[0] + self._coefficients[1] * delta_p + self._coefficients[2] * delta_p**2 + self._coefficients[3] * delta_p**3 + self._coefficients[4] * delta_p**4
