9e778d32bb643be6b25690592cb4c68ed088f5e2
Carmelo Pellegrino <carmelo.pellegrino@gmail.com>
2020-05-03 00:44:40 +0200

    first draft of the calibration function added
    it is the callback for both the Start Calibration and Retry buttons

--- gui/calibration/spirometer.py
+++ gui/calibration/spirometer.py
@@ -29,6 +29,9 @@ class SpirometerCalibration(QtWidgets.QWidget):
         self._mainwindow = None
         self._coefficients = []
 
+        self.retry_button.setEnabled(False)
+        self.retry_button.clicked.connect(self._start_calibration)
+        self.start_calibration.clicked.connect(self._start_calibration)
         self.back_button.clicked.connect(self._accept_values)
 
     def connect_mainwindow_esp32(self, mainwindow, esp32):
@@ -49,3 +52,38 @@ class SpirometerCalibration(QtWidgets.QWidget):
                             self._coefficients[index])
 
             self._mainwindow.show_startup()
+
+    def _start_calibration(self):
+        """
+        Start retrieving data to fit.
+        """
+
+        self.start_calibration.setEnabled(False)
+        self.back_button.setEnabled(False)
+        self.retry_button.setEnabled(False)
+        self.completion_bar.setValue(0)
+        self._coefficients = []
+        self.endstatus_label.setText("")
+
+        try:
+            calibrator = self._esp32.venturi_calibration()
+
+            flows = []
+            delta_ps = []
+            for completion, flow, delta_p in calibrator.data():
+                self.completion_bar.setValue(completion)
+                flows.append(flow)
+                delta_ps.append(delta_p)
+
+            # TODO call the function to do the regression
+            # self._coefficients =
+            # if self._coefficients == []:
+            #     raise Exception("invalid data points")
+            self.endstatus_label.setText("Succeeded")
+        except: #pylint: disable=W0702
+            self.start_calibration.setEnabled(True)
+            self.retry_button.setEnabled(True)
+            self.endstatus_label.setText("Failed")
+        finally:
+            self.back_button.setEnabled(True)
+            del calibrator
