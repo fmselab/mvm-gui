45dc93009415546228e5c44a32ee1f52a48ed83a
javier <javier.romualdez@gmail.com>
2020-05-12 18:46:45 -0400

    Added catch for error state when exiting

--- gui/mvm_gui.py
+++ gui/mvm_gui.py
@@ -87,7 +87,11 @@ def main():
     watchdog.timeout.connect(watchdog_redirect)
     watchdog.start(config["wdinterval"] * 1000)
     app.exec_()
-    esp32.set("wdenable", 0)
+    if esp32.except_state:
+        input("Press any key to contiue...")
+    else:
+        esp32.set("wdenable", 0)
+    
 
 
 if __name__ == "__main__":
